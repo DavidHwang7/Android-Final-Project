fc38fe9d0cabc4909505175cf65944f0c867eb90
DavidHwang7 <40482355+DavidHwang7@users.noreply.github.com>
2018-12-20 02:12:41 +0800

    layout change

--- app/src/main/java/davidhwang/raiwayvillage/Home.kt
+++ app/src/main/java/davidhwang/raiwayvillage/Home.kt
@@ -6,6 +6,9 @@ import android.os.Bundle
 class Home : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val actionBar = supportActionBar
+//ActionBar actionBar = getActionBar();
+        actionBar!!.hide()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
     }
