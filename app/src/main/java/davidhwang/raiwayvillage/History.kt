23fbfda643315e53de60b43112512e8104d00e1c
Candy <46012786+ctLo98124@users.noreply.github.com>
2018-12-21 01:44:09 +0800

    新增history

---
+++ app/src/main/java/davidhwang/raiwayvillage/History.kt
@@ -0,0 +1,14 @@
+package davidhwang.raiwayvillage
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class History : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_history)
+    }
+
+
+}
