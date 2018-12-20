4d814e6acadd5924c00495dc9e23a024dff687df
DavidHwang7 <40482355+DavidHwang7@users.noreply.github.com>
2018-12-20 23:23:27 +0800

    按鈕動畫成功(變亮變暗)

--- app/src/main/java/davidhwang/raiwayvillage/Home.kt
+++ app/src/main/java/davidhwang/raiwayvillage/Home.kt
@@ -2,17 +2,27 @@ package davidhwang.raiwayvillage
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class Home : AppCompatActivity() {
-
-	//Hello
-//Hello from David
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val actionBar = supportActionBar
 //ActionBar actionBar = getActionBar();
-        actionBar!!.hide()//#303F9FDark  //#3F51B5Primary
+        actionBar!!.hide() //#303F9FDark  //#3F51B5Primary
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_fhd)
+
+        val intro_button: ImageButton = findViewById(R.id.intro_button)
+        val camera_button: ImageButton = findViewById(R.id.camera_button)
+        val info_button: ImageButton = findViewById(R.id.info_button)
+        val history_button: ImageButton = findViewById(R.id.history_button)
+
+        camera_button.setOnTouchListener(ButtonTouchLight())
+        intro_button.setOnTouchListener(ButtonTouchDark())
+        info_button.setOnTouchListener(ButtonTouchDark())
+        history_button.setOnTouchListener(ButtonTouchLight())
+
+
     }
 }
