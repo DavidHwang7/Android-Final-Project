917a009769e8106ccdc0b3b4b29006dca098a684
Candy <46012786+ctLo98124@users.noreply.github.com>
2018-12-21 11:31:17 +0800

    add history home button

--- app/src/main/java/davidhwang/raiwayvillage/History.kt
+++ app/src/main/java/davidhwang/raiwayvillage/History.kt
@@ -1,14 +1,25 @@
 package davidhwang.raiwayvillage
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class History : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_history)
+        val home: ImageButton = findViewById(R.id.back_history)
+        home.setOnTouchListener(ButtonTouchDark())
+        home.setOnClickListener(android.view.View.OnClickListener() { accessHome() })
     }
 
+    private fun accessHome(){
+        val intent = Intent()
+        intent.setClass(this,
+                Home::class.java)
+        startActivity(intent)
+    }
 
 }
