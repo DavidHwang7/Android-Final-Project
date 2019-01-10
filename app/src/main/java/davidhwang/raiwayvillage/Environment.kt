8a1de4dce34324e562d797c7e5b1d89925362ae0
Candy <46012786+ctLo98124@users.noreply.github.com>
2019-01-11 00:25:13 +0800

    env待修改

--- app/src/main/java/davidhwang/raiwayvillage/Environment.kt
+++ app/src/main/java/davidhwang/raiwayvillage/Environment.kt
@@ -18,8 +18,6 @@ import android.widget.Toast
 
 class Environment : AppCompatActivity() {
 
-    private val mSpn: Spinner? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_environment)
@@ -30,35 +28,36 @@ class Environment : AppCompatActivity() {
 
         val mSpn : Spinner = findViewById<View>(R.id.spn) as Spinner
 
-        val arrAdapSpn = ArrayAdapter.createFromResource(this, //對應的Context
+        val arrAdapSpn = ArrayAdapter.createFromResource(this@Environment, //對應的Context
                 R.array.env, //選項資料內容
                 R.layout.spinner_item) //自訂getView()介面格式(Spinner介面未展開時的View)
 
         arrAdapSpn.setDropDownViewResource(R.layout.spinner_dropdown_item) //自訂getDropDownView()介面格式(Spinner介面展開時，View所使用的每個item格式)
         mSpn.adapter = arrAdapSpn //將宣告好的 Adapter 設定給 Spinner
-
+        mSpn.setSelection(0,true)
         mSpn.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(p0: AdapterView<*>?) {
+                /*Toast.makeText(this@Environment,
+                        R.string.input_error,
+                        Toast.LENGTH_SHORT).show()*/
             }
 
             override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
                 if(position==0){
-                    access_0()
+                    access0()
                 }else if(position==1){
-                    access_1()
+                    access1()
                 }else if(position==2){
-                    access_2()
+                    access2()
                 }else if(position==3){
-                    access_3()
+                    access3()
                 }else if(position==4){
-                    access_4()
+                    access4()
                 }
             }
-
         }
 
     }
-
 
     private fun accessHome(){
         val intent = Intent()
@@ -67,35 +66,35 @@ class Environment : AppCompatActivity() {
         startActivity(intent)
     }
 
-    private fun access_0(){
+    private fun access0(){
         val intent = Intent()
         intent.setClass(this,
                 env0::class.java)
         startActivity(intent)
     }
 
-    private fun access_1(){
+    private fun access1(){
         val intent = Intent()
         intent.setClass(this,
                 env1::class.java)
         startActivity(intent)
     }
 
-    private fun access_2(){
+    private fun access2(){
         val intent = Intent()
         intent.setClass(this,
                 env2::class.java)
         startActivity(intent)
     }
 
-    private fun access_3(){
+    private fun access3(){
         val intent = Intent()
         intent.setClass(this,
                 env3::class.java)
         startActivity(intent)
     }
 
-    private fun access_4(){
+    private fun access4(){
         val intent = Intent()
         intent.setClass(this,
                 env4::class.java)
--- app/src/main/java/davidhwang/raiwayvillage/env0.kt
+++ app/src/main/java/davidhwang/raiwayvillage/env0.kt
@@ -2,11 +2,13 @@ package davidhwang.raiwayvillage
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import davidhwang.raiwayvillage.R.id.toolbar
 
 class env0 : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_env0)
+
     }
 }
