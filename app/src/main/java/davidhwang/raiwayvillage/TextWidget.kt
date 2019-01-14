e41adf14c1ca2ca996a7063f1dbbf13dc37c381f
Candy <46012786+ctLo98124@users.noreply.github.com>
2019-01-14 17:05:51 +0800

    widget更新

--- app/src/main/java/davidhwang/raiwayvillage/TextWidget.kt
+++ app/src/main/java/davidhwang/raiwayvillage/TextWidget.kt
@@ -1,14 +1,20 @@
 package davidhwang.raiwayvillage
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
+import android.util.Log
 import android.widget.RemoteViews
 
 /**
  * Implementation of App Widget functionality.
  * App Widget Configuration implemented in [TextWidgetConfigureActivity]
  */
+
+const val WIDGET_SYNC = "WIDGET_SYNC"
+
 class TextWidget : AppWidgetProvider() {
 
 
@@ -34,24 +40,37 @@ class TextWidget : AppWidgetProvider() {
         // Enter relevant functionality for when the last widget is disabled
     }
 
+    override fun onReceive(context: Context, intent: Intent?) {
+        //Log.i("receive", "Test");
+        if(WIDGET_SYNC == intent?.action) {
+            val appWidgetId = intent.getIntExtra("appWidgetId",0)
+            updateAppWidget(context, AppWidgetManager.getInstance(context), appWidgetId)
+        }
+        super.onReceive(context, intent)
+    }
+
     companion object {
 
         internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager,
                                      appWidgetId: Int) {
-
+            Log.i("update", "Test");
+            val intent = Intent(context,TextWidget::class.java)
+            intent.action = WIDGET_SYNC
+            intent.putExtra("appWidgetId",appWidgetId)
+            val pendingIntent = PendingIntent.getBroadcast(context,0,intent,0)
+            // Construct the RemoteViews object
+            val views = RemoteViews(context.packageName, R.layout.text_widget)
             val list1 = listOf<String>(
                     "面對陽光，黑暗就會在背後",
-                    "我不怕千萬人阻擋，只怕自己投降",
+                    "我不怕他人阻擋，只怕自己投降",
                     "人之所以能，是相信能",
                     "不要等待機會而要創造機會",
                     "自信的生命最美麗",
                     "含淚播種的人一定能含笑收穫")
-
-            val widgetText = list1[0]
-            // Construct the RemoteViews object
-            val views = RemoteViews(context.packageName, R.layout.text_widget)
-            views.setTextViewText(R.id.appwidget_text, widgetText)
-
+            var i : Int = (0..5).shuffled().first()
+            views.setTextViewText(R.id.appwidget_text, list1[i])
+            Log.i("list", "Test");
+            views.setOnClickPendingIntent(R.id.iv_sync,pendingIntent)
             // Instruct the widget manager to update the widget
             appWidgetManager.updateAppWidget(appWidgetId, views)
         }
